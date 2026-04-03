89971bc009631c280e656467c9e53a81596acb68
Antonio <Antonio@10.9.168.189>
2026-04-03 20:34:00 +0800

    CISC3003-Practice10-2026APR03

---
+++ CISC3003-Practice10-2026APR03/cisc3003-practice10.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>dc22697 Chen Joaquin Antonio PHP</title>
+</head>
+<body>
+	<h1>Chen Joaquin Antonio PHP</h1>
+	<p>The SHA256 hash of "Chen Joaquin Antonio" is
+	<?php 
+	   echo  hash('sha256', 'Chen Joaquin Antonio');
+	?></p>
+	<pre>ASCII ART:
+       ____       _      _    
+      / ___|     | |    / \   
+     | |      _  | |   / _ \  
+     | |___  | |_| |  / ___ \ 
+      \____|  \___/  /_/   \_\
+	</pre>
+	<a href="check.php">Click here to check the error setting</a>
+	<br/>
+	<a href="fail.php">Click here to cause a traceback</a>
+</body>
